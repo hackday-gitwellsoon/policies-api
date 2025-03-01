45ebf1a89bf59ad0f07547c8aede0e5dafe36cac
Marcus <TiplerMF@cardiff.ac.uk>
2025-03-01 14:38:35 +0000

    imports

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 	return "Hello World!"
+
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
