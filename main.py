f6b44ae3169f3280ce6837655efc14bdbf584409
Marcus <TiplerMF@cardiff.ac.uk>
2025-03-01 16:20:31 +0000

    Hello World but none working example

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, make_response, session, g
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 app = Flask(__name__)
 
@@ -16,17 +19,14 @@ class documents(db.Model):
 	_id = db.Column("Policy_ID", db.Integer, primary_key=True)
 	title = db.Column("Title", db.String)
 	description = db.Column("Description", db.String)
-
-
-test = documents.query.all()
-
-@app.before_request
-def pres():
-	print(test)
+	def __init__(self, title, description):
+		self.title = title
+		self.description = description
 
 @app.route('/')
 def hello():
-	return
+	test = documents.query.filter_by(_id = 1).first()
+	return "hello world"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
