1a28cb9f152431c87ffe9357d32e6a00aafebd66
Marcus <TiplerMF@cardiff.ac.uk>
2025-03-01 14:45:05 +0000

    Database Init

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 
 app = Flask(__name__)
+SECRET_KEY = "Preschool3-Duration5-Strum1-Tigress0-Shove8-Scenic3-Unaligned6-Nerd7-Reforest0-Produce8"
+app.config['SECRET_KEY'] = SECRET_KEY
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class technologies(db.Model):
+	_id = db.Column("id", db.Integer, primary_key=True)
 
 @app.route('/')
 def hello():
 	return "Hello World!"
-
 
 
 if __name__ == '__main__':
